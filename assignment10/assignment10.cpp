0508c293299e072d29d8a9103ee76087a395cf5b
nissimlavy <nissim.lavy1@gmail.com>
2014-05-20 12:55:17 -0400

    Update assignment10.cpp

--- assignment10/assignment10.cpp
+++ assignment10/assignment10.cpp
@@ -1,64 +1,161 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 
 #define N 10
 
 using namespace std;
-printCust(string);
-sortName(string);
-sortSales(string);
-int main() {
 
-	ifstream file;
-	file.open("file.txt");
-	
-	cust arr[N];
-	string line;
-	
-	//file >> cust.name.first >> cust.name.last >> cust.state.state >> cust.salesHist.sales[1] >> cust.salesHist.sales[2] >> cust.salesHist.sales[3] >> cust.units.units;
-	while(!file.eof()){
-		getline(file, line)
-		printCust(line);
-		sortName(line);
-		printCust(line);
-		sortSales(line);
-		printCust(line);
-	}
-	
-	cout << "PROGRAM FINISHED" << endl;
-	return 0;
-	
-}
+void printCust(string);
+void sortName(string);
+void sortSales(string);
+
+class Name {
+    public:
+
+        string first;
+        string last;
+};
+
+class State {
+    public:
+
+        string state;
+};
+
+class SalesHist {
+    public:
+
+        double sale1, sale2, sale3;
+        double total(){
+            return sale1 + sale2 + sale3;
+        }
+};
+
+class Units{
+    public:
+
+        int u;
+};
 
 class cust {
-	public:
-		
-		double totalSales;
-		
-		class name {
-			public:
-				
-				string first;
-				string last;
-			};
-			
-		class state {
-			public:
-				
-				string state;
-		};
-		
-		class salesHist {
-			public:
-			
-				double sales[];
-		};
-		
-		class units{
-			public:
-			
-				int units;
-		};
+    public:
 
+	    Units units;
+	    Name name;
+	    State st;
+	    SalesHist salesHist;
 };
+
+void printcust(cust cu[N], int nelts)
+{
+for(int i=0;i<nelts;i++)
+   {
+
+       cout << cu[i].name.first << "\t" << cu[i].name.last << "\t"
+            << cu[i].st.state
+            << "\t" << cu[i].salesHist.sale1 << "\t" << cu[i].salesHist.sale2 << "\t" << cu[i].salesHist.sale3 << "\t"
+            << cu[i].units.u << endl;
+
+   }
+
+}
+
+void sortname(cust cu[N], int nelts){
+
+    string temp,temp2,temp3;
+    int temp4=0,temp5=0;
+    
+    for(int pass=1;pass<nelts;pass++)
+
+        for(int i=0;i<nelts-1;i++)
+
+            if (cu[i].name.last>cu[i+1].name.last){
+                temp=cu[i].name.last;
+                cu[i].name.last=cu[i+1].name.last;
+                cu[i+1].name.last=temp;
+
+                temp2=cu[i].name.first;
+                cu[i].name.first=cu[i+1].name.first;
+                cu[i+1].name.first=temp2;
+                
+                temp3=cu[i].st.state;
+                cu[i].st.state=cu[i+1].st.state;
+                cu[i+1].st.state=temp3;
+                
+                temp4=cu[i].units.u;
+                cu[i].units.u=cu[i+1].units.u;
+                cu[i+1].units.u=temp4;
+                
+                temp5=cu[i].salesHist.total();
+                cu[i].salesHist.total()==cu[i+1].salesHist.total();
+                cu[i+1].salesHist.total()==temp5;
+
+            }
+return;
+}
+
+
+
+void sortsales(cust cu[N], int nelts)
+{
+   string temp,temp2,temp3;
+   int hold=0,temp4=0;
+
+   for(int pass=1; pass < nelts; pass++)
+
+       for(int i=0; i< nelts-1; i++)
+
+           if (cu[i].salesHist.total()<cu[i+1].salesHist.total()) {
+               hold=cu[i+1].salesHist.total();
+               cu[i].salesHist.total()==cu[i+1].salesHist.total();
+               cu[i+1].salesHist.total()==hold;
+    
+               temp2=cu[i].name.first;
+               cu[i].name.first=cu[i+1].name.first;
+               cu[i+1].name.first=temp2;
+    
+               temp=cu[i].name.last;
+               cu[i].name.last=cu[i+1].name.last;
+               cu[i+1].name.last=temp;
+    
+               temp3=cu[i].st.state;
+               cu[i].st.state=cu[i+1].st.state;
+               cu[i+1].st.state=temp3;
+    
+               temp4=cu[i].units.u;
+               cu[i].units.u=cu[i+1].units.u;
+               cu[i+1].units.u=temp4;
+            }
+    return;
+
+}
+
+
+int main()
+{
+    cust cu[N];
+    ifstream file;
+
+    file.open("input.txt");
+    int i=0;
+    while(!file.eof()){
+
+        file >> cu[i].name.first >> cu[i].name.last >> cu[i].st.state >> cu[i].salesHist.sale1 >> cu[i].salesHist.sale2 >> cu[i].salesHist.sale3 >> cu[i].units.u;
+
+        i++;
+   }
+
+    printcust(cu, i);
+    cout<<endl<<endl;
+    sortname(cu, i); // sort by last name
+    printcust(cu, i);
+    cout<<endl<<endl;
+    sortsales(cu, i);
+    printcust(cu, i);
+
+
+    cout << "PROGRAM FINISHED" << endl;
+    return 0;
+}
